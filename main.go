5b193c42896b23361f451179f6261f680322797a
raphaelmorsch <raphaelmorsch@intersystem.eti.br>
2021-09-03 23:59:33 +0200

    update recipe endpoint

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
+	router.PUT("/recipes/:id", UpdateReceiptHandler)
 	router.Run()
 }
 
@@ -52,4 +53,33 @@ func NewRecipeHandler(c *gin.Context) {
 
 func ListRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
+}
+
+func UpdateReceiptHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+		return
+	}
+
+	recipes[index] = recipe
+	recipes[index].ID = id
+	c.JSON(http.StatusOK, recipe)
 }
